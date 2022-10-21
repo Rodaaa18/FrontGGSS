271ab9b00b140f3e497602f82de3f4e5a5c40631
Rodrigo <rodrigo@inft.com.ar>
2022-10-21 14:32:11 -0300

    Refactoring

---
+++ src/components/Buttons/ButtonLarge.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ButtonLarge = ({color, tamaño, justyfy, nameButton}) => {
+  return (
+        <button className={`btn btn-${color} btn-${tamaño} d-md-flex justify-content-${justyfy}`}>{nameButton}</button>
+    
+  )
+}
+
+export default ButtonLarge
